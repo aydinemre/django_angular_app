7957a1fcf21ce7dfc1f25e776dffd487ae51f3e1
Emre AYDIN <eaydin@boynergrup.com>
2019-10-06 13:53:50 +0300

    OneToOne Relation

--- demo/admin.py
+++ demo/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from demo.models import Book
+from demo.models import Book, BookNumber
+
 
 # admin.site.register(Book)
 
@@ -10,3 +11,6 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ["title", "description"]
     list_filter = ["published"]
     search_fields = ["title", "description"]
+
+
+admin.site.register(BookNumber)
